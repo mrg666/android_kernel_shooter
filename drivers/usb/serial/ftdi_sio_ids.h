8d730cb16fa13d7e93cf6bd955b5fd04769f04d9
Ozan Çağlayan <ozancag@gmail.com>
2012-08-10 17:25:10 +0300

    USB: ftdi_sio: Add VID/PID for Kondo Serial USB
    commit 7724a1edbe463b06d4e7831a41149ba095b16c53 upstream.
    
    This adds VID/PID for Kondo Kagaku Co. Ltd. Serial USB Adapter
    interface:
    http://www.kondo-robot.com/EN/wp/?cat=28
    
    Tested by controlling an RCB3 board using libRCB3.
    
    Signed-off-by: Ozan Çağlayan <ozancag@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Conflicts:
    	drivers/usb/serial/ftdi_sio.c

--- drivers/usb/serial/ftdi_sio_ids.h
+++ drivers/usb/serial/ftdi_sio_ids.h
@@ -786,6 +786,13 @@
 #define RTSYSTEMS_CT29B_PID		0x9e54	/* CT29B Radio Cable */
 
 /*
+ * Kondo Kagaku Co.Ltd.
+ * http://www.kondo-robot.com/EN
+ */
+#define KONDO_VID 		0x165c
+#define KONDO_USB_SERIAL_PID	0x0002
+
+/*
  * Bayer Ascensia Contour blood glucose meter USB-converter cable.
  * http://winglucofacts.com/cables/
  */

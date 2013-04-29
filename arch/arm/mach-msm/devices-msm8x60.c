f63bad98e54f220a012ab6f4bb67fee1b3240b8b
mrg666 <drgungor@hotmail.com>
2013-04-29 13:57:46 -0400

    cleanup GPU overclock code

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -1005,9 +1005,9 @@ static struct resource kgsl_3d0_resources[] = {
 	},
 };
 
-#ifdef CONFIG_GPU_OVERCLOCK
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
+#ifdef CONFIG_GPU_OVERCLOCK
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 4,
@@ -1018,34 +1018,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 3,
 			.io_fraction = 0,
 		},
-		{
-			.gpu_freq = 200000000,
-			.bus_freq = 2,
-			.io_fraction = 100,
-		},
-		{
-			.gpu_freq = 177778000,
-			.bus_freq = 1,
-			.io_fraction = 100,
-		},
-		{
-			.gpu_freq = 27000000,
-			.bus_freq = 0,
-		},
-	},
-	.init_level = 0,
-	.num_levels = 5,
-	.set_grp_async = NULL,
-	.idle_timeout = HZ/5,
-	.nap_allowed = true,
-	.clk_map = KGSL_CLK_CORE | KGSL_CLK_IFACE | KGSL_CLK_MEM_IFACE,
-#ifdef CONFIG_MSM_BUS_SCALING
-	.bus_scale_table = &grp3d_bus_scale_pdata,
-#endif
-};
 #else
-static struct kgsl_device_platform_data kgsl_3d0_pdata = {
-	.pwrlevel = {
 		{
 			.gpu_freq = 266667000,
 			.bus_freq = 4,
@@ -1056,6 +1029,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 3,
 			.io_fraction = 33,
 		},
+#endif
 		{
 			.gpu_freq = 200000000,
 			.bus_freq = 2,
@@ -1081,7 +1055,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.bus_scale_table = &grp3d_bus_scale_pdata,
 #endif
 };
-#endif
 
 struct platform_device msm_kgsl_3d0 = {
 	.name = "kgsl-3d0",

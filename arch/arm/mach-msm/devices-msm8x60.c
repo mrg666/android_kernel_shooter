d911bf27958bfb06421a281fe1b56ed1e5534ce1
mrg666 <drgungor@hotmail.com>
2013-04-30 10:20:15 -0400

    GPU overclock touch up

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -850,7 +850,7 @@ static struct msm_bus_vectors grp2d0_nominal_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1156),
+		.ib = KGSL_CONVERT_TO_MBPS(990),
 	},
 };
 
@@ -881,7 +881,7 @@ static struct msm_bus_paths grp2d0_bus_scale_usecases[] = {
 #ifdef CONFIG_GPU_OVERCLOCK	
 	{
 		ARRAY_SIZE(grp2d0_nominal_low_vectors),
-		grp2d0_nominal_vectors,
+		grp2d0_nominal_low_vectors,
 	},
 	{
 		ARRAY_SIZE(grp2d0_nominal_vectors),
@@ -924,7 +924,7 @@ static struct msm_bus_vectors grp2d1_nominal_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(1156),
+		.ib = KGSL_CONVERT_TO_MBPS(990),
 	},
 };
 
@@ -955,7 +955,7 @@ static struct msm_bus_paths grp2d1_bus_scale_usecases[] = {
 #ifdef CONFIG_GPU_OVERCLOCK	
 	{
 		ARRAY_SIZE(grp2d1_nominal_low_vectors),
-		grp2d1_nominal_vectors,
+		grp2d1_nominal_low_vectors,
 	},
 	{
 		ARRAY_SIZE(grp2d1_nominal_vectors),
@@ -1036,7 +1036,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 100,
 		},
 		{
-			.gpu_freq = 177778000,
+			.gpu_freq = 160000000,
 			.bus_freq = 1,
 			.io_fraction = 100,
 		},
@@ -1089,10 +1089,18 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 			.bus_freq = 3,
 		},
 		{
-			.gpu_freq = 228571000,
+			.gpu_freq = 200000000,
 			.bus_freq = 2,
 		},
-#endif
+		{
+			.gpu_freq = 160000000,
+			.bus_freq = 1,
+		},
+		{
+			.gpu_freq = 160000000,
+			.bus_freq = 0,
+		},
+#else
 		{
 			.gpu_freq = 200000000,
 			.bus_freq = 1,
@@ -1101,12 +1109,12 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 			.gpu_freq = 200000000,
 			.bus_freq = 0,
 		},
+#endif
 	},
+	.init_level = 0,
 #ifdef CONFIG_GPU_OVERCLOCK
-	.init_level = 1,
 	.num_levels = 4,
 #else
-	.init_level = 0,
 	.num_levels = 2,
 #endif
 	.set_grp_async = NULL,
@@ -1151,10 +1159,18 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 			.bus_freq = 3,
 		},
 		{
-			.gpu_freq = 228571000,
+			.gpu_freq = 200000000,
 			.bus_freq = 2,
 		},
-#endif
+		{
+			.gpu_freq = 160000000,
+			.bus_freq = 1,
+		},
+		{
+			.gpu_freq = 160000000,
+			.bus_freq = 0,
+		},
+#else
 		{
 			.gpu_freq = 200000000,
 			.bus_freq = 1,
@@ -1163,12 +1179,12 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 			.gpu_freq = 200000000,
 			.bus_freq = 0,
 		},
+#endif
 	},
+	.init_level = 0,
 #ifdef CONFIG_GPU_OVERCLOCK
-	.init_level = 1,
 	.num_levels = 4,
 #else
-	.init_level = 0,
 	.num_levels = 2,
 #endif
 	.set_grp_async = NULL,

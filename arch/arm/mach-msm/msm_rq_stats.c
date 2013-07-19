79462943e6eb4ea010f34bc3edee2747d939bddc
mrg666 <drgungor@hotmail.com>
2013-07-18 21:24:10 -0400

    msm_mpdecision: directly access rq_stats and eliminate msm_rq_stats The original purpose of msm_rq_stats is enabling userspace access to rq_stats and comes with the overhead of additional spin/mutex locks and sysfs facilities,  mainly used by userspace mpdecision. However, kernel-based mpdecision still depends on this code to obtain cpu load averages while using a tiny fraction of the code. This commit completely eliminates msm_rq_stats code and adds a very simple rq_stats direct-access capability in msm_mpdecision.

--- arch/arm/mach-msm/msm_rq_stats.c
+++ arch/arm/mach-msm/msm_rq_stats.c
@@ -55,24 +55,6 @@ struct cpu_load_data {
 
 static DEFINE_PER_CPU(struct cpu_load_data, cpuload);
 
-#ifdef CONFIG_MSM_MPDEC
-unsigned int get_rq_avg(void)
-{
-	unsigned long flags = 0;
-        unsigned int rq = 0;
-
-        spin_lock_irqsave(&rq_lock, flags);
-
-        rq = rq_info.rq_avg;
-        rq_info.rq_avg = 0;
-
-        spin_unlock_irqrestore(&rq_lock, flags);
-
-        return rq;
-}
-EXPORT_SYMBOL(get_rq_avg);
-#endif
-
 static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
 {
 	u64 idle_time;
